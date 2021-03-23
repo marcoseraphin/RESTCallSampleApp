8ebc63ddbf813ea87d183bac4f97df664ead1cf6
Marco Seraphin <seraphin@develope-it.de>
2021-03-23 10:42:15 +0100

    DataTemplateSelector added

---
+++ RESTSampleApp/RESTSampleApp/CompleteStatusTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RESTSampleApp
+{
+    public class CompleteStatusTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate CompleteTemplate { get; set; }
+        public DataTemplate UnCompleteTemplate { get; set; }
+
+        public CompleteStatusTemplateSelector()
+        {
+            this.CompleteTemplate = new DataTemplate(typeof(ViewCellCompleted));
+            this.UnCompleteTemplate = new DataTemplate(typeof(ViewCellUncompleted));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            ToDoItem toDoItem = item as ToDoItem;
+
+            if (toDoItem.Completed)
+            {
+                return this.CompleteTemplate;
+            }
+
+            return this.UnCompleteTemplate;
+        }
+    }
+}
---
+++ RESTSampleApp/RESTSampleApp/ViewCellUncompleted.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RESTSampleApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewCellUncompleted : ViewCell
+    {
+        public ViewCellUncompleted()
+        {
+            InitializeComponent();
+        }
+    }
+}
