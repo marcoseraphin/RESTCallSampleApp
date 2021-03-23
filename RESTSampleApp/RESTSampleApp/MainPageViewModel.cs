cf3e94323133fe94873552f0af61a20c59b219ba
Marco Seraphin <seraphin@develope-it.de>
2021-03-23 10:02:45 +0100

    Projektdateien hinzufügen.

---
+++ RESTSampleApp/RESTSampleApp/MainPageViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RESTSampleApp
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public Command CallRESTCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    try
+                    {
+                       await this.CallRESTService();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                        throw;
+                    }
+                }
+                );
+            }
+        }
+
+        private ObservableCollection<ToDoItem> toDoItemList;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<ToDoItem> ToDoItemList
+        {
+            get
+            {
+                return this.toDoItemList;
+            }
+            set
+            {
+                this.toDoItemList = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("ToDoItemList"));
+            }
+        }
+
+        private async Task<bool> CallRESTService()
+        {
+            var uri = new Uri("https://jsonplaceholder.typicode.com/todos");
+
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(uri);
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var json = await httpResponseMessage.Content.ReadAsStringAsync();
+                if (!String.IsNullOrEmpty(json))
+                {
+                    List<ToDoItem> toDoItems = JsonConvert.DeserializeObject<List<ToDoItem>>(json);
+                    this.ToDoItemList = new ObservableCollection<ToDoItem>(toDoItems); 
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ RESTSampleApp/RESTSampleApp/ToDoItem.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTSampleApp
+{
+    public class ToDoItem
+    {
+        [JsonProperty("userId")]
+        public long UserId { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("completed")]
+        public bool Completed { get; set; }
+
+    }
+}
